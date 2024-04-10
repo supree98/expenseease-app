574682bf7d5c934e5922230066a9c46d1ef76bad
Supreeth P <supreethpgowda007@gmail.com>
2024-04-10 14:47:10 +0530

    Setup Group Service

---
+++ expenseease-group-svc/src/main/java/com/expenseease/group/ExpenseeaseGroupSvcApplication.java
@@ -0,0 +1,13 @@
+package com.expenseease.group;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseeaseGroupSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseeaseGroupSvcApplication.class, args);
+	}
+
+}
