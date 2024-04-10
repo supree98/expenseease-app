42bb4da1fdb5887edfcf7a3c97d8b1c35245a915
Supreeth P <supreethpgowda007@gmail.com>
2024-04-10 14:29:21 +0530

    Set IAM svc

---
+++ expenseease-iam-svc/src/test/java/com/expenseease/iam/ExpenseEaseIamServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expenseease.iam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseEaseIamServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
